53dda1791412ac0568455a9a0e9eb8f50e6ed799
Rosan <1634979604@qq.com>
2026-05-31 19:56:24 +0800

    feat: scaffold Spring Boot backend service

---
+++ rdvp-backend/src/main/java/com/rmf/rdvp/RdvpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rmf.rdvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdvpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RdvpBackendApplication.class, args);
+    }
+
+}
---
+++ rdvp-backend/src/test/java/com/rmf/rdvp/RdvpBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package com.rmf.rdvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class RdvpBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
